d02579e0443742d18635058c9a6cae061a85667e
leidyolinto <suverleide.olinto@gmail.com>
2023-07-12 11:38:16 -0300

    Criando container

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,31 @@
-import Head from 'next/head'
-import Navbar from '@/components/navbar/navbar'
-
+import Head from "next/head";
+import Navbar from "@/components/navbar/navbar";
+import Subtitle from "@/tipography/subtitle/subtitle";
+import Container from "@/container/container";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>DevSteam: A sua loja online de games</title>
-        <meta name="description" content="DevSteam: A sua loja online de games" />
+        <meta
+          name="description"
+          content="DevSteam: A sua loja online de games"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-       <Navbar/>
+        <Navbar />
+        <Container>
+          <div>
+            <Subtitle> Promocões </Subtitle>
+          </div>
+          <div>
+            <Subtitle> Meus jogos </Subtitle>
+          </div>
+        </Container>
       </div>
     </>
-  )
+  );
 }
